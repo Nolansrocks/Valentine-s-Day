2a48bd9cfaa7ff2994f16c5974388dd8b3095852
Nolansrocks <dylanservia16@gmail.com>
2026-02-14 02:06:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function checkPassword(){
+  const pass = document.getElementById("password").value;
 
+  if(pass === "13-04-2025"){
+    document.getElementById("login-screen").style.display = "none";
+    document.getElementById("main-content").classList.remove("hidden");
+
+    const music = document.getElementById("music");
+    music.play();
+  }else{
+    document.getElementById("error").innerText = "Fecha incorrecta 😢";
+  }
+}
