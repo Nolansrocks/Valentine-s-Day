6c545931aeb65c6218bde91b5e5151bc4ee7c98a
Nolansrocks <dylanservia16@gmail.com>
2026-02-14 02:34:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,23 @@ function checkPassword(){
 
     iniciarMusica();
     crearCorazones();
-    escribirTexto();
+    escribirMensaje1();
   }else{
     document.getElementById("error").innerText = "Fecha incorrecta 😢";
   }
 }
 
-/* música fade */
+/* 🎵 MUSICA LOOP CON FADE */
 function iniciarMusica(){
   const music = document.getElementById("music");
+  music.loop = true;
   music.volume = 0;
   music.play().catch(()=>{});
 
   let vol = 0;
   const fade = setInterval(()=>{
     if(vol < 1){
-      vol += 0.05;
+      vol += 0.03;
       music.volume = vol;
     }else{
       clearInterval(fade);
@@ -30,7 +31,7 @@ function iniciarMusica(){
   },200);
 }
 
-/* corazones */
+/* ❤️ CORAZONES */
 function crearCorazones(){
   const cont = document.querySelector(".floating-hearts");
 
@@ -46,33 +47,18 @@ function crearCorazones(){
   },300);
 }
 
-/* texto maquina escribir */
-const texto = `Lo siento por los inicios malos, las malas mañanas o las peleas por estupideces, 
+/* ✍️ TEXTOS */
+const texto1 = `Lo siento por los inicios malos, las malas mañanas o las peleas por estupideces, 
 pero te prometo que en cada momento de la vida, en cada latido de mi corazon hay una parte tuya que siempre resuena en mi, 
 puede que no sea el mas listo, ni el mas apto, ni el mas fuerte, pero te prometo que por vos aprendo libros de memoria, 
 me preparo para cualquier situacion y muevo montañas enteras porque sos esa chispa que enciende mis metas 
 y nunca nadie te podra igualar...`;
 
-function escribirTexto(){
-  let i=0;
-  const velocidad = 35;
-  const cont = document.getElementById("mensaje");
-
-  function escribir(){
-    if(i < texto.length){
-      cont.innerHTML += texto.charAt(i);
-      i++;
-      setTimeout(escribir, velocidad);
-    }
-  }
-  escribir();
-}
-const texto1 = "Feliz San Valentín mi amor ❤️";
-
 const textoFinal = `Entre problemas y mundos destruidos voy a extender mi mano para que puedas alzarte y ver que el mundo entero es tuyo, que los buenos momentos los armamos juntos, que la historia que armamos nosotros prevalecerá y al final estaremos los dos brillando como nunca lo hicimos antes, para que mires atras y veas que yo voy a estar para vos en todo momento y que cuando mires hacia adelante tambien voy a estar yo esperandote para siempre dar el siguiente paso juntos, te amo con el alma candu, siempre para vos dylan <3`;
 
 let escribiendo = false;
 
+/* maquina escribir */
 function escribirTexto(texto, callback){
   const cont = document.getElementById("mensaje");
   cont.innerHTML = "";
@@ -92,12 +78,14 @@ function escribirTexto(texto, callback){
   escribir();
 }
 
+/* primer texto */
 function escribirMensaje1(){
   escribirTexto(texto1, ()=>{
     document.getElementById("siguienteBtn").style.display="inline-block";
   });
 }
 
+/* boton siguiente */
 function siguienteTexto(){
   if(escribiendo) return;
 
